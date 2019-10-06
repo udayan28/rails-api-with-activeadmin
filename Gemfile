9302b5568133bf82aa80e0eb48bd0f4ac16e5581
uda5150 <tks.udagawa@gmail.com>
2019-10-07 03:34:27 +0900

    active adminの導入

--- Gemfile
+++ Gemfile
@@ -28,6 +28,10 @@ gem 'bootsnap', '>= 1.4.2', require: false
 # Seed管理
 gem 'seed-fu'
 
+# ActiveAdmin関連
+gem 'activeadmin'
+gem 'devise'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,11 @@ module RailsApiWithActiveadmin
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Active Admin用
+    config.middleware.use Rack::MethodOverride
+    config.middleware.use ActionDispatch::Flash
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
   end
 end
